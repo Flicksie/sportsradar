a334b2f322c6acaed1f24ba6a07ba3b12bb4ea12
Lucas Reis <flicky@pollux.gg>
2024-04-04 15:41:02 +0200

    add summary test

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -15,5 +15,20 @@ describe('Scoreboard', () => {
         expect(home_score).toBe(0);
     });
 
+    it('should get a summary of games in progress ordered by their total score', () => {
+        const game1 = scoreboard.startGame('Home1', 'Away1');
+        scoreboard.updateScore(game1.id, 1, 2);
+
+        const game2 = scoreboard.startGame('Home2', 'Away2');
+        scoreboard.updateScore(game2.id, 3, 4);
+
+        const game3 = scoreboard.startGame('Home3', 'Away3');
+        scoreboard.updateScore(game3.id, 1, 2);
+
+        const summary_ids = scoreboard.getSummary().map(game => game.id);
+
+
+        expect(summary_ids).toEqual([2, 3, 1]);
+      });
 
 });
