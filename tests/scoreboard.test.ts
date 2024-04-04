9e4889c692605ddffc29cd4ac069c8b62ab5777e
Lucas Reis <flicky@pollux.gg>
2024-04-04 15:40:42 +0200

    add initial test

--- tests/scoreboard.test.ts
+++ tests/scoreboard.test.ts
@@ -7,4 +7,13 @@ describe('Scoreboard', () => {
         scoreboard = new Scoreboard();
     });
 
+    it('new game score is 0 - 0', () => {
+        const current_game = scoreboard.startGame('Home', 'Away');
+        const { away_score, home_score } = current_game;
+
+        expect(away_score).toBe(0);
+        expect(home_score).toBe(0);
+    });
+
+
 });
