59e1740cd3176cf4f559071140b80620aecfcdcf
Lucas Reis <flicky@pollux.gg>
2024-04-04 15:40:07 +0200

    add scoreboard test

---
+++ tests/scoreboard.test.ts
@@ -0,0 +1,10 @@
+import  Scoreboard  from '../src/scoreboard';
+
+describe('Scoreboard', () => {
+    let scoreboard: Scoreboard;
+
+    beforeEach(() => {
+        scoreboard = new Scoreboard();
+    });
+
+});
